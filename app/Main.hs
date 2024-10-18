2bc2ef55ac3ad1556d5e3b3143497d742694d179
wtjerry <wtjerry123@gmail.com>
2024-10-18 14:16:31 +0200

    extend examples

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,10 @@ module Main (main) where
 import           Lib
 
 main :: IO ()
-main = someFunc
+main = do
+    someFunc
+    listPhoneSpecs
+    printWritingInstrument examplePencil
 
 
 
@@ -12,13 +15,31 @@ data PhoneSpecification = PhoneSpecification
     , weight         :: Int
     , publishingYear :: Int
     , ram            :: Double }
+    deriving Show
 
 type SharpnessLevel = Int
-type Length = Int
+type LeftoverLength = Int
 type LeftoverInk = Int
 
 data WritingInstrument
-    = Pencil SharpnessLevel Length
+    = Pencil SharpnessLevel LeftoverLength
     | Pen LeftoverInk
     | Brush
     | HammerAndChisel SharpnessLevel
+
+listPhoneSpecs :: IO ()
+listPhoneSpecs =
+    let phone = PhoneSpecification "IPhone 27" 1234 2029 3
+    in do
+        print phone
+
+
+printWritingInstrument :: WritingInstrument -> IO ()
+printWritingInstrument (Pencil sharpnessLevel leftoverLength) = putStrLn ("Pencil: Sharpness: " <> show sharpnessLevel <> " with length " <> show leftoverLength)
+printWritingInstrument (Pen leftoverInk) = putStrLn ("Pen: leftover ink: " <> show leftoverInk)
+printWritingInstrument Brush = putStrLn "Brush"
+printWritingInstrument (HammerAndChisel sharpnessLevel) = putStrLn ("Pencil: Sharpness: " <> show sharpnessLevel)
+
+
+examplePencil :: WritingInstrument
+examplePencil = Pencil 100 2
