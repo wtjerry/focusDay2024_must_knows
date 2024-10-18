1e293a270a84937d75627ecf5bea4af67527b349
wtjerry <wtjerry123@gmail.com>
2024-10-18 13:53:41 +0200

    add examples for modeling section of session

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,24 @@
 module Main (main) where
 
-import Lib
+import           Lib
 
 main :: IO ()
 main = someFunc
+
+
+
+data PhoneSpecification = PhoneSpecification
+    { model          :: String
+    , weight         :: Int
+    , publishingYear :: Int
+    , ram            :: Double }
+
+type SharpnessLevel = Int
+type Length = Int
+type LeftoverInk = Int
+
+data WritingInstrument
+    = Pencil SharpnessLevel Length
+    | Pen LeftoverInk
+    | Brush
+    | HammerAndChisel SharpnessLevel
